afb27bca5c7f364659b07851f258ff17ec2d0307
JarrettR <jrainier@gmail.com>
2014-09-22 19:22:53 -0700

    Added non-blocking serial receive, longer auth string

--- vhs-door-v3/vhs_door_opener.ino
+++ vhs-door-v3/vhs_door_opener.ino
@@ -1,55 +1,107 @@
-// VHS dopor opener
+// VHS door opener
 #include "HardwareSerial.h"
 
-unsigned char disengagePin = 2;
-unsigned char engagePin = 3;
+unsigned char disengagePin = 3;
+unsigned char engagePin = 2;
 
-void engage() 
-{
+
+char inData[20]; // Allocate some space for the string
+char inChar=-1; // Where to store the character read
+byte index = 0; // Index into array; where to store the character
+
+#define BUFF_SIZE 6
+
+#define DEBUG 1
+
+void engage() {
+  if( DEBUG ) Serial1.println( "Engage..." );
   digitalWrite(engagePin, HIGH);
   delay(100);
   digitalWrite(engagePin, LOW);
 }
 
-void disengage() 
-{
+void disengage() {
+  if( DEBUG ) Serial1.println( "Disengage..." );
   digitalWrite(disengagePin, HIGH);
   delay(100);
   digitalWrite(disengagePin, LOW);
 }
 
-void enablecontrol()
-{
+void enablecontrol() {
+  if( DEBUG ) Serial1.println( "Enable control..." );
   engage();
   delay(10000);
   disengage();
+  Serial1.println( "opened" );
 }
 
+int counter = 0;
 
-void setup() 
-{
-  Serial.begin(2400);
+void setup() {
+//  Serial1.begin(2400);
+  Serial1.begin( 2400 );
+
+  if( DEBUG ) Serial1.println( "Initializing pins..." );
+  pinMode( engagePin, OUTPUT );     
+  pinMode( disengagePin, OUTPUT ); 
+
+  if( DEBUG ) Serial1.println( "Bringing up lock..." );
+  //enablecontrol();
   
-  pinMode(engagePin, OUTPUT);     
-  pinMode(disengagePin, OUTPUT); 
-
-  enablecontrol();  
+  Serial1.println( "ONLINE" );
 }
 
-void loop() 
+int readNb(char *buffer, int length, int timeout)
 {
-   
-    if (Serial.available() > 0)
+    long         previousMillis = 0;
+    int          bread          = 0;
+    char         c;
+    
+    previousMillis = millis();
+    while((millis()-previousMillis)<timeout)
     {
-        unsigned char command = Serial.read();
-        switch(command)
+        if(Serial1.available()>0)
         {
-            case 'e':
+            c             = Serial1.read();
+            if(bread>length)
             {
-                enablecontrol();
-                break;
+                return(-1); // string received is too long
             }
+            else
+            {
+                buffer[bread]=c;
+            }
+            bread++;
         }
     }
+    return(bread);
+}
 
+
+void loop() {
+
+
+  if (Serial1.available() > 0) {
+    char c = Serial1.read();
+        Serial1.write(c);
+    if(c == '*') {
+      Serial1.write("Ctrl Char");
+      if(readNb(inData, 6, 1000) == -1) {
+        Serial1.write("Too long");
+      } else {
+        Serial1.write("Next");
+        if(strcmp(inData,"sesame")  == 0){
+          Serial1.write("Open!");
+          enablecontrol();
+        } else {
+          Serial1.write("Not Open!");
+          Serial1.write(inData);
+        }
+      }
+    }
+  }
+    for(int i=0;i<7;i++){
+      inData[i]=0;
+    }
+        
 }
